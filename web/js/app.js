5dbcd426125db60fa6f24b2cf862c3ae67e51c3b
jeanval <jv.lassoie@gmail.com>
2017-01-03 08:33:49 +0100

    version finale

--- web/js/app.js
+++ web/js/app.js
@@ -1,49 +1,65 @@
 'use strict';
 var app = angular.module('app',['ui.router','ngAnimate']);
 
-app.config(function($urlRouterProvider, $stateProvider) {
-	$urlRouterProvider.otherwise('/');
+app.run(function($rootScope) {
+	$rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams) {
+		$rootScope.load = true;
 
-	$stateProvider.state('home', {
-		url : '/',
-		templateUrl : Routing.generate('loadTpl', { tpl: 'home' }),
-		controller : 'appCtrl'	
-	})
-	.state('lettre', {
-		url : '/lettre',
-		templateUrl : Routing.generate('loadTpl', { tpl: 'lettre' }),
-		controller : 'lettreCtrl'	
-	})
-	.state('cv', {
-		url : '/cv',
-		templateUrl : Routing.generate('loadTpl', { tpl: 'cv' }),
-		controller : 'cvCtrl'	
 	});
 
-});
+	$rootScope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams) {
+		$rootScope.load = false;
+	});
+})
 
 
-app.controller('appCtrl',function($scope,$timeout) {
+
+	app.config(function($urlRouterProvider, $stateProvider) {
+		$urlRouterProvider.otherwise('/');
+
+		$stateProvider.state('home', {
+			url : '/',
+			templateUrl : Routing.generate('loadTpl', { tpl: 'home' }),
+			controller : 'appCtrl'
+		})
+		.state('lettre', {
+			url : '/lettre',
+			templateUrl : Routing.generate('loadTpl', { tpl: 'lettre' }),
+			controller : 'lettreCtrl'
+		})
+		.state('cv', {
+			url : '/cv',
+			templateUrl : Routing.generate('loadTpl', { tpl: 'cv' }),
+			controller : 'cvCtrl'
+		});
+
+	});
+
+	app.controller('appCtrl',function($rootScope,$scope,$timeout) {
+
 	$timeout(function(){
 
 		$scope.animation = "animation";
 	}, 1);
 
+
 });
 
-app.controller('lettreCtrl',function($scope,$timeout) {
-	$timeout(function(){
-
-		$scope.animation = "animation";
-	}, 1);
+	app.controller('lettreCtrl',function($rootScope,$scope,$timeout) {
 	
-});
+		$timeout(function(){
 
-app.controller('cvCtrl',function($scope,$timeout) {
-	$timeout(function(){
+			$scope.animation = "animation";
+		}, 1);
 
-		$scope.animation = "animation";
-	}, 1);
+	});
 
+	app.controller('cvCtrl',function($rootScope,$scope,$timeout) {
 	
-});
+		$timeout(function(){
+
+			$scope.animation = "animation";
+		}, 1);
+
+
+	});
